580726ad02c7f02e9b9418350ac9333bee566d83
Markus Koechl <markus.koechl@autodesk.com>
2024-04-17 17:45:47 +0200

    Add project files.

---
+++ VDS-Editor-2025-CAD/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace VDS_Editor_2025_CAD
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ VDS-Editor-2025-Vault/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace VDS_Editor_2025_Vault
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
